a5e940b003f396f4839fe5a4386b329920672525
ztimson <zaktimson@gmail.com>
2025-12-27 14:36:06 -0500

    Fixed exec helper return

--- src/cli.ts
+++ src/cli.ts
@@ -1,11 +1,18 @@
-import {exec, execSync} from 'child_process';
+import {execSync} from 'child_process';
+import {spawn} from 'node:child_process';
 
 export function $(str: TemplateStringsArray, ...args: string[]): Promise<string> {
 	let cmd = str.reduce((acc, part, i) => acc + part + (args[i] || ''), '');
-	return new Promise((res, rej) => exec(cmd, (err, stdout, stderr) => {
-		if(err) return rej(stderr || err);
-		return res(stdout);
-	}))
+	return new Promise((res, rej) => {
+		const proc = spawn(cmd, {shell: true});
+		let stdout = '', stderr = '';
+		proc.stdout.on('data', (data) => stdout += data);
+		proc.stderr.on('data', (data) => stderr += data);
+		proc.on('close', (code) => {
+			if(code !== 0) return rej(new Error(stderr || stdout));
+			res(stdout.trim());
+		});
+	});
 }
 
 export function $Sync(str: TemplateStringsArray, ...args: string[]): string {
